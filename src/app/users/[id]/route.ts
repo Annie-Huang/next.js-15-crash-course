d983a982f357706f7bae402ecab53e0d31270c5c
Annie Huang <annie.hn@gmail.com>
2025-02-08 22:59:37 +1100

    Finally set up prettier

--- src/app/users/[id]/route.ts
+++ src/app/users/[id]/route.ts
@@ -1,8 +1,8 @@
-import { users } from "../route";
+import { users } from '../route';
 
 export async function GET(
   _request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: string } },
 ) {
   const { id } = await params;
   const user = users.find((user) => user.id === parseInt(id));
--- src/app/users/route.ts
+++ src/app/users/route.ts
@@ -1,6 +1,6 @@
 export const users = [
-  { id: 1, name: "John Doe" },
-  { id: 2, name: "Jane Doe" },
+  { id: 1, name: 'John Doe' },
+  { id: 2, name: 'Jane Doe' },
 ];
 
 export async function GET() {
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
   const newUser = { id: users.length + 1, name: user.name };
   users.push(newUser);
   return new Response(JSON.stringify(newUser), {
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
     status: 201,
   });
 }

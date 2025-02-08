2349f107b6435ab061a629a41075360fcf4ace7d
Annie Huang <annie.hn@gmail.com>
2025-02-08 15:46:22 +1100

    Have different style on the <Link> if the current page matches the link

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,15 +1,36 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export const Navigation = () => {
+  // check the current path
+  const pathname = usePathname();
+
   return (
     <nav className='flex justify-center items-center p-4'>
-      <Link href='/' className='text-blue-500 mr-4'>
+      <Link
+        href='/'
+        className={pathname === '/' ? 'font-bold mr-4' : 'text-blue-500 mr-4'}
+      >
         Home
       </Link>
-      <Link href='/about' className='text-blue-500 mr-4'>
+      <Link
+        href='/about'
+        className={
+          pathname === '/about' ? 'font-bold mr-4' : 'text-blue-500 mr-4'
+        }
+      >
         About
       </Link>
-      <Link href='/products/1' className='text-blue-500 mr-4'>
+      <Link
+        href='/products/1'
+        className={
+          pathname.startsWith('/products/1')
+            ? 'font-bold mr-4'
+            : 'text-blue-500 mr-4'
+        }
+      >
         Product 1
       </Link>
     </nav>

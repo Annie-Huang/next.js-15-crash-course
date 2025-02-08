4348b5badd2c1117aaf57fb19b5e5394e06baac7
Annie Huang <annie.hn@gmail.com>
2025-02-08 15:51:26 +1100

    Programmatically navigate to certain path (router.push from 'next/navigation')

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,21 @@
-const Page = () => {
-  return <h1>About Us</h1>;
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+const About = () => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>About Us</h1>
+      <button
+        onClick={() => router.push('/')}
+        className='bg-blue-500 text-white p-2 rounded-md'
+      >
+        Go to Home
+      </button>
+    </div>
+  );
 };
 
-export default Page;
+export default About;
--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export const Navigation = () => {
-  // check the current path
+  // check the current path (active path)
   const pathname = usePathname();
 
   return (
